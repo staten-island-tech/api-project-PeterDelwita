1ca7d61f32ba13c4bc0a462209c40874785b2446
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2024-11-19 14:00:58 -0500

    tryingtofindstatus

--- app/JS/main.js
+++ app/JS/main.js
@@ -6,7 +6,7 @@ async function getAPI() {
   try {
     const response = await fetch(
       "https://data.cityofnewyork.us/resource/f9bf-2cp4.json"
-    );
+    ); // Make the guard clause; find an API with a status of 200 or 299
   } catch (error) {
     alert("I could not find that agent");
   }
