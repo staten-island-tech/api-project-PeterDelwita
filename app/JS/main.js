201ce44cf7d6821e83a44cd8356ec0cbc6489144
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2024-11-19 13:34:22 -0500

    ddasdsadsadasad

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,2 +1,13 @@
 import "../css/style.css";
-// Possible API: https://data.cityofnewyork.us/resource/f9bf-2cp4.json
+
+// No status shown on API. Maybe try another
+
+async function getAPI() {
+  try {
+    const response = await fetch(
+      "https://data.cityofnewyork.us/resource/f9bf-2cp4.json"
+    );
+  } catch (error) {
+    alert("I could not find that agent");
+  }
+}
