678b7a937b6f2c8fd47b88d79966c6ef0319b160
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2024-11-20 14:01:30 -0500

    Update main.js

--- app/JS/main.js
+++ app/JS/main.js
@@ -7,7 +7,12 @@ async function getAPI() {
     const response = await fetch(
       "https://data.cityofnewyork.us/resource/f9bf-2cp4.json"
     ); // Make the guard clause; find an API with a status of 200 or 299
+    const data = await response.json();
+    console.log(data);
+    // Log names, scores of each school.
   } catch (error) {
-    alert("I could not find that agent");
+    alert("I could not find those schools");
   }
 }
+
+getAPI();
