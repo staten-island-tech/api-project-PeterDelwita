f742482fc595855f8c148533decec53c37a3df46
PeterDelwita <124073562+PeterDelwita@users.noreply.github.com>
2024-11-21 13:58:10 -0500

    async

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,18 +1,20 @@
-import "../css/style.css";
-
 // No status shown on API. Maybe try another
+const URL = "https://data.cityofnewyork.us/resource/f9bf-2cp4.json";
 
-async function getAPI() {
+async function getData(URL) {
   try {
-    const response = await fetch(
-      "https://data.cityofnewyork.us/resource/f9bf-2cp4.json"
-    ); // Make the guard clause; find an API with a status of 200 or 299
-    const data = await response.json();
-    console.log(data);
+    const response = await fetch(URL); // Make the guard clause; find an API with a status of 200 or 299
+    console.log(response); // Have to log this to see status
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      console.log(data);
+    }
     // Log names, scores of each school.
   } catch (error) {
-    alert("I could not find those schools");
+    alert("School not found"); // I plan to take the informaiton and insert it as HTML
   }
 }
 
-getAPI();
+getData(URL);
