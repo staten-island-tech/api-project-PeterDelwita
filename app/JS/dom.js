2bee13d73cff9c69c6e71aac5fc6c22306b5e49e
PeterDelwita <peterd72@nycstudents.net>
2024-11-25 23:41:26 -0500

    buttons, gotta make form

--- app/JS/dom.js
+++ app/JS/dom.js
@@ -1,7 +1,12 @@
 const DOMSelectors = {
   container: document.querySelector(".container"),
-  button: document.querySelector(".button"),
-  card: document.querySelector(".card")
+  button: document.querySelectorAll(".btn"),
+  criticalReadingButton: document.querySelector("#critical-reading"),
+  writingButton: document.querySelector("#writing"),
+  mathButton: document.querySelector("#math"),
+  applyEverythingButton: document.querySelector("#all"),
+  resetFiltersButton: document.querySelector("#reset"),
+  card: document.querySelector(".card"),
 };
 
 export { DOMSelectors };
--- app/JS/main.js
+++ app/JS/main.js
@@ -1,3 +1,4 @@
+import "../css/style.css";
 import { DOMSelectors } from "./dom.js";
 
 const URL = "https://data.cityofnewyork.us/resource/f9bf-2cp4.json";
@@ -22,33 +23,98 @@ async function getData(URL) {
 getData(URL);
 // How about a button showing the next 150? Previous/next button? (if possible)
 
-// What to do with this data...
-// Center it around SAT scores. Make a list of schools, add filters for SAT scores, let users sort schools by DBN or school name
-// Lots of schools; cards are tacky.
-function loadSchools() {
+let readingThreshold = 200;
+let writingThreshold = 200;
+let mathThreshold = 200;
+
+function createCards(filterMode) {
   // Load schools and SAT scores. How about a list??
-  getData(URL);
-  data.forEach((school) =>
-    DOMSelectors.container.insertAdjacentHTML(
-      // Make some cards
-      "beforeend",
-      `
-      <div id="list">
+  data
+    .filter((school) => {
+      if ((filterMode = "reading")) {
+        school.sat_critical_reading_avg_score >= readingThreshold;
+      } else if ((filterMode = "writing")) {
+        school.sat_writing_avg_score >= writingThreshold;
+      } else if ((filterMode = "math")) {
+        school.sat_math_avg_score >= mathThreshold;
+      } else if ((filterMode = "all")) {
+        school.sat_critical_reading_avg_score >= readingThreshold;
+        school.sat_writing_avg_score >= writingThreshold;
+        school.sat_math_avg_score >= mathThreshold;
+      } else if ((filterMode = "")) {
+        school.dbn.includes("");
+      } else {
+        return;
+      }
+    })
+    .forEach((school) => {
+      const cardHTML = `
+      <div class="card h-80 w-[22%] border-3 border-black rounded-sm m-8 p-8 flex flex-wrap justify-center items-center">
         <h2 class="text 2xl font-sans" id="school-name">${school.school_name}</h2>
-        <h3 id="dbn">${school.dbn}</h2>
-        <ul id="sat-scores">
+        <h3 class="dbn">${school.dbn}</h2>
+        <ul class="sat-avg-scores">
           <li id="math-scores">${school.sat_math_avg_score}</li>
           <li id="writing-scores">${school.sat_writing_avg_score}</li>
           <li id="reading-scores">${school.sat_critical_reading_avg_score}</li>
           <li id="test-takers">${school.num_of_sat_test_takers}</li>
       </div>
-  `
-    )
-  );
+  `;
+      DOMSelectors.container.insertAdjacentHTML(
+        // Make some cards
+        "beforeend",
+        cardHTML
+      );
+    });
 }
 
-loadSchools();
+createCards("");
 
+// Cards for buttons
+
+function filterByReading() {
+  DOMSelectors.container.innerHTML("");
+  createCards("reading");
+}
+
+function filterByWriting() {
+  DOMSelectors.container.innnerHTML("");
+  createCards("writing");
+}
+
+function filterByMath() {
+  DOMSelectors.container.innerHTML("");
+  createCards("math");
+}
+
+function filterByAll() {
+  DOMSelectors.container.innerHTML("");
+  createCards("all");
+}
+
+function resetFilters() {
+  DOMSelectors.container.innerHTML("");
+  createCards("");
+}
+
+DOMSelectors.criticalReadingButton.addEventListener("click", function () {
+  filterByReading();
+});
+
+DOMSelectors.writingButton.addEventListener("click", function () {
+  filterByWriting();
+});
+
+DOMSelectors.mathButton.addEventListener("click", function () {
+  filterByMath();
+});
+
+DOMSelectors.applyEverythingButton.addEventListener("click", function () {
+  filterByAll();
+});
+
+DOMSelectors.resetFiltersButton.addEventListener("click", function () {
+  resetFilters();
+});
 // Plan:
 // - Make a form allowing the user to input math, critical reading, and writing scores and test takers
 // - Filter thresholds will change based on what the user inputs. Text should change once user submits form
